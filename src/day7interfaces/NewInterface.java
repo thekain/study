9e2572cbe28b198c084880a73ddb3a36d3e2386b
Amilo <Amilo@Amilo-PC>
2015-06-22 22:40:17 +0600

    first commit

---
+++ src/day7interfaces/NewInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day7interfaces;
+
+/**
+ *
+ * @author Amilo
+ */
+public interface NewInterface {
+    public void texter(String word);
+    public void initInterface(NewJFrame1 context);
+}
