e130cf62c80ba4724419603dcee5c589d036d789
cnp.ak <cnp.ak@TECH-PC>
2015-06-30 16:46:36 +0600

    changed JBextended class. Back to system.out.

--- src/day9plus1homeworkv2/SearchForm.java
+++ src/day9plus1homeworkv2/SearchForm.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -89,17 +90,32 @@ public class SearchForm extends javax.swing.JFrame {
         label = new javax.swing.JLabel();
         textField = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        outputTable = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        textField.setText("jTextField1");
+        textField.setText("asus");
 
-        jButton1.setText("jButton1");
+        jButton1.setText("Мне повезет!");
         jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jButton1MouseClicked(evt);
             }
         });
+
+        outputTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(outputTable);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -108,10 +124,17 @@ public class SearchForm extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(label, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton1)
-                    .addComponent(textField, javax.swing.GroupLayout.PREFERRED_SIZE, 413, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(955, 966, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1)
+                            .addComponent(label, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(textField, javax.swing.GroupLayout.PREFERRED_SIZE, 263, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -120,30 +143,63 @@ public class SearchForm extends javax.swing.JFrame {
                 .addComponent(textField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addGap(45, 45, 45)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
                 .addComponent(label, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(21, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+        outputTable.setAutoResizeMode(outputTable.AUTO_RESIZE_OFF);
         String searchItem = textField.getText();
-
         for (int i = 0; i < arrayList.size(); i++) {
 //            if (arrayList.get(i).description.indexOf(searchItem)!=-1){
 //                label.setText(label.getText() + arrayList.get(i).id + " " + arrayList.get(i).description + " " + arrayList.get(i).priceR + " " + arrayList.get(i).priceO + " " + arrayList.get(i).warranty);
 //            }
-
             if (arrayList.get(i).description.toLowerCase().indexOf(searchItem.toLowerCase()) != -1) {
                 label.setText(label.getText() + arrayList.get(i).id + " " + arrayList.get(i).description + " " + arrayList.get(i).priceR + " " + arrayList.get(i).priceO + " " + arrayList.get(i).warranty);
             }
-
             //System.out.println(arrayList.get(i).id + " " + arrayList.get(i).description + " " + arrayList.get(i).priceR + " " + arrayList.get(i).priceO + " " + arrayList.get(i).warranty);
             //label.setText(label.getText()+arrayList.get(i).id + " " + arrayList.get(i).description + " " + arrayList.get(i).priceR + " " + arrayList.get(i).priceO + " " + arrayList.get(i).warranty);
             //label.setText(label.getText() + arrayList.get(i).id + " " + arrayList.get(i).description + " " + arrayList.get(i).priceR + " " + arrayList.get(i).priceO + " " + arrayList.get(i).warranty);
         }
+
+        Object[] columnNames = {"ID", "Описание", "Цена Р", "Цена О", "Гарантия"};
+        DefaultTableModel model = new DefaultTableModel(new Object[0][0], columnNames) {
+            @Override
+            public Class getColumnClass(int column) {
+                Class returnValue;
+                if ((column >= 0) && (column < getColumnCount())) {
+                    returnValue = getValueAt(0, column).getClass();
+                } else {
+                    returnValue = Object.class;
+                }
+                return returnValue;
+            }
+
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (arrayList.get(i).description.toLowerCase().indexOf(searchItem.toLowerCase()) != -1) {
+            Object[] o = new Object[8];
+            o[0] = arrayList.get(i).id;
+            o[1] = arrayList.get(i).description;
+            o[2] = arrayList.get(i).priceR;
+            o[3] = arrayList.get(i).priceO;
+            o[4] = arrayList.get(i).warranty;
+            model.addRow(o);}
+        }
+        outputTable.setModel(model);
+
+
     }//GEN-LAST:event_jButton1MouseClicked
 
     /**
@@ -183,7 +239,9 @@ public class SearchForm extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel label;
+    private javax.swing.JTable outputTable;
     private javax.swing.JTextField textField;
     // End of variables declaration//GEN-END:variables
 }
