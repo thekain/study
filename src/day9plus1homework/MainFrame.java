00b09045661a7f48184f05e05ae8ba70a33b17c2
cnp.ak <cnp.ak@TECH-PC>
2015-06-30 10:21:01 +0600

    changed JBextended class. Back to system.out.

--- src/day9plus1homework/MainFrame.java
+++ src/day9plus1homework/MainFrame.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package day9plus1;
+package day9plus1homework;
 
+import day9plus1.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -147,6 +148,7 @@ public class MainFrame extends javax.swing.JFrame {
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
